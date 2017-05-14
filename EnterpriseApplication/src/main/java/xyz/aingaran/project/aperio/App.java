f520557ac721c0568e44a4d56a1538b0c8b24eb8
Aingaran Elango <eaingaran@gmail.com>
2017-05-14 16:04:19 +0530

    Project Structure commit
    Signed-off-by: Aingaran Elango <eaingaran@gmail.com>

---
+++ EnterpriseApplication/src/main/java/xyz/aingaran/project/aperio/App.java
@@ -0,0 +1,13 @@
+package xyz.aingaran.project.aperio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
